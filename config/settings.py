12c570c722010117a87dfa882e548a09d537ae65
potpot7 <121911598+potpot7@users.noreply.github.com>
2023-02-22 18:36:10 +0900

    sta

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR/'/static/')
+# STATIC_ROOT = os.path.join(BASE_DIR/'/static/')
+STATIC_ROOT = 'https://heartmatch.onrender.com/static'
 STATICFILES_DIRS = [
     BASE_DIR.joinpath('static'),
 ]
