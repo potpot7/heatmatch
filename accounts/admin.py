3acbda05fb328a9557559944ef93982bde732602
potpot7 <121911598+potpot7@users.noreply.github.com>
2023-02-22 16:05:08 +0900

    20230222

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-from .models import CustomUser,Account,Profile
-
-CustomUser = get_user_model()
-
-admin.site.register(CustomUser)
-admin.site.register(Account)
-admin.site.register(Profile)
--- friend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Friend,Message,Posts
-
-# Register your models here.
-
-admin.site.register(Friend)
-admin.site.register(Message)
-admin.site.register(Posts)
