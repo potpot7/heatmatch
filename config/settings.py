fd3b34cf9218dea2cf5fec86d366585b1e81513c
potpot7 <121911598+potpot7@users.noreply.github.com>
2023-02-24 23:44:10 +0900

    modified

--- config/settings.py
+++ config/settings.py
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 DATABASES = {
-    # 'default': {
-    # 'ENGINE': 'django.db.backends.sqlite3',
-    # 'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default':dj_database_url.config(),
+    'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default':dj_database_url.config(),
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': env('DB_NAME'),
--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('accounts.urls')),
+    path('', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('accounts/', include('accounts.urls')),
     path('q_a/', include('q_a.urls')),
     path('friend/', include('friend.urls')),
 ]
